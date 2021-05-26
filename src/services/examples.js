336f6d9ba89afdd5082a07e284ad36501feba0ac
Ryan Mehta <ryan.mehta@gmail.com>
2021-05-26 07:09:01 -0700

    update marked

--- src/services/examples.js
+++ src/services/examples.js
@@ -1,6 +1,6 @@
 import { BSON } from "realm-web";
-import { fetchExplainer } from "../utils/examples";
 import { getCollection } from "./db";
+import marked from "marked";
 
 export const findExamples = async (query) => {
   const collection = await getCollection("examples");
@@ -13,10 +13,8 @@ export const findExampleById = async (_id) => {
   const collection = await getCollection("examples");
   const example = await collection.findOne({ _id: BSON.ObjectID(_id) });
 
-  const explainer = await fetchExplainer(example);
-
   return {
     ...example,
-    explainer,
+    explainer: marked(example.explainer),
   };
 };
